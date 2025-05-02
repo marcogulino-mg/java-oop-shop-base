97740d57f00194f2733dc055897e97a444a2cc7e
marcogulino-mg <marcogulino98@gmail.com>
2025-05-02 17:37:17 +0200

    [feat] add Attributes and Construct method in class Prodotto

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,6 +2,6 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,21 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+    // Attributes
+    public int code;
+    public String name;
+    public String description;
+    public float price;
+    public int iva;
 
+    // Methods
+
+    // Construct
+    public Prodotto(int code, String name, String description, float price, int iva) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
 }
